76f68d5de6f9a2b235d97f78852c92f3c7f8c14e
dursuninel <drsinl1905@gmail.com>
2023-04-30 23:14:16 +0300

    .

--- Airport.UI/wwwroot/paneljs/app.js
+++ Airport.UI/wwwroot/paneljs/app.js
@@ -70,68 +70,17 @@ $("#addMyCarForm").validate({
 
 
 
-$("#updateMyCarForm").validate({
+
+
+
+$("#location_step1").validate({
     rules: {
-        Brand: {
+        place_Id: {
             required: true,
-        },
-        Model: {
-            required: true,
-        },
-        Series: {
-            required: true,
-        },
-        Trim: {
-            required: true,
-        },
-        Class: {
-            required: true,
-        },
-        Type: {
-            required: true,
-        },
-        MaxPasseng: {
-            required: true,
-        },
-        SuitCase: {
-            required: true,
-        },
-        SmallBags: {
-            required: true,
-        },
-        Service: {
-            required: true,
-        },
+        }
     },
     messages: {
-        Brand: {
-            required: "This Field is Required",
-        },
-        Model: {
-            required: "This Field is Required",
-        },
-        Series: {
-            required: "This Field is Required",
-        },
-        Trim: {
-            required: "This Field is Required",
-        },
-        Class: {
-            required: "This Field is Required",
-        },
-        Type: {
-            required: "This Field is Required",
-        },
-        MaxPasseng: {
-            required: "This Field is Required",
-        },
-        SuitCase: {
-            required: "This Field is Required",
-        },
-        SmallBags: {
-            required: "This Field is Required",
-        },
-        Service: {
+        place_Id: {
             required: "This Field is Required",
         },
     },
@@ -139,6 +88,76 @@ $("#updateMyCarForm").validate({
         form.submit();
     },
 });
+
+//$("#updateMyCarForm").validate({
+//    rules: {
+//        Brand: {
+//            required: true,
+//        },
+//        Model: {
+//            required: true,
+//        },
+//        Series: {
+//            required: true,
+//        },
+//        Trim: {
+//            required: true,
+//        },
+//        Class: {
+//            required: true,
+//        },
+//        Type: {
+//            required: true,
+//        },
+//        MaxPasseng: {
+//            required: true,
+//        },
+//        SuitCase: {
+//            required: true,
+//        },
+//        SmallBags: {
+//            required: true,
+//        },
+//        Service: {
+//            required: true,
+//        },
+//    },
+//    messages: {
+//        Brand: {
+//            required: "This Field is Required",
+//        },
+//        Model: {
+//            required: "This Field is Required",
+//        },
+//        Series: {
+//            required: "This Field is Required",
+//        },
+//        Trim: {
+//            required: "This Field is Required",
+//        },
+//        Class: {
+//            required: "This Field is Required",
+//        },
+//        Type: {
+//            required: "This Field is Required",
+//        },
+//        MaxPasseng: {
+//            required: "This Field is Required",
+//        },
+//        SuitCase: {
+//            required: "This Field is Required",
+//        },
+//        SmallBags: {
+//            required: "This Field is Required",
+//        },
+//        Service: {
+//            required: "This Field is Required",
+//        },
+//    },
+//    submitHandler: function (form) {
+//        form.submit();
+//    },
+//});
 
 
 
