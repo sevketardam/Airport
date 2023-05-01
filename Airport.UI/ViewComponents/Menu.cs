4a3e57d2afb838e04ecccb2d97a98cf0ae12299e
Şevket Arda Mülazımoğlu <arda2002@windowslive.com>
2023-05-02 01:37:40 +0300

    .

---
+++ Airport.UI/ViewComponents/Menu.cs
@@ -0,0 +1,28 @@
+﻿using Airport.DBEntitiesDAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Airport.UI.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        IUserDatasDAL _user;
+        public Menu(IUserDatasDAL user)
+        {
+            _user = user;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+            var user = _user.SelectByID(userId);
+            if (user != null)
+            {
+                return View(user);
+            }
+            return null;
+        }
+    }
+}
