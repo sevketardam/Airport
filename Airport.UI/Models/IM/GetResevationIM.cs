b4e66b05bb7f97763086337a573e5814dbe8e42e
Şevket Arda Mülazimoğlu <47789105+sevketardam@users.noreply.github.com>
2023-05-12 02:12:40 +0300

    .

--- Airport.UI/Models/IM/GetResevationIM.cs
+++ Airport.UI/Models/IM/GetResevationIM.cs
@@ -5,7 +5,9 @@ namespace Airport.UI.Models.IM
     public class GetResevationIM
     {
         public string PickValue { get; set; }
+        public string PickValueText { get; set; }
         public string DropValue { get; set; }
+        public string DropValueText { get; set; }
         public DateTime FlightTime { get; set; }
         public int PeopleCount { get; set; }
         public bool ReturnStatus { get; set; }
