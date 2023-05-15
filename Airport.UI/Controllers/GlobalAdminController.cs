edf6305c701370cf3def184dc0f98463b937cc13
Şevket Arda Mülazımoğlu <arda2002@windowslive.com>
2023-05-15 14:15:35 +0300

    .

--- Airport.UI/Controllers/GlobalAdminController.cs
+++ Airport.UI/Controllers/GlobalAdminController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Airport.DBEntities.Entities;
+using Airport.DBEntitiesDAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Airport.UI.Controllers
 {
     public class GlobalAdminController : Controller
     {
+        ICouponsDAL _coupons;
+        public GlobalAdminController(ICouponsDAL coupons)
+        {
+            _coupons = coupons;
+        }
+
+        [HttpGet("panel/coupons")]
         public IActionResult CreateCoupon()
         {
-            return View();
+
+            var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+            var myCoupons = _coupons.SelectByFunc(a=>a.UserId == userId);
+            return View(myCoupons);
+        }
+
+        [HttpPost]
+        public JsonResult CreateCouponJSON(Coupons coupon)
+        {
+            try
+            {
+                if (coupon != null)
+                {
+                    var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+                    _coupons.Insert(new Coupons
+                    {
+                        Active = coupon.Active,
+                        Comment = coupon.Comment,
+                        CouponCode = coupon.CouponCode,
+                        CouponFinishDate = coupon.CouponFinishDate,
+                        CouponStartDate = coupon.CouponStartDate,
+                        CouponLimit = coupon.CouponLimit,
+                        CreatedDate = DateTime.Now,
+                        IsPerma = coupon.IsPerma,
+                        Discount = coupon.Discount,
+                        UserId = userId
+                    });
+                    return new JsonResult(new { result = 1 });
+                }
+                return new JsonResult(new { result = 2 });
+            }
+            catch (Exception)
+            {
+                return new JsonResult(new { });
+            }
         }
     }
 }
