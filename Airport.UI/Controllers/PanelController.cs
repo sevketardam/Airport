1b8d9383461a05e23601419e61d22c9c8fd9dae2
Şevket Arda Mülazimoğlu <47789105+sevketardam@users.noreply.github.com>
2023-05-30 20:41:27 +0300

    .

--- Airport.UI/Controllers/PanelController.cs
+++ Airport.UI/Controllers/PanelController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System;
 using System.Security.Claims;
 using Airport.UI.Models.Interface;
+using Airport.DBEntities.Entities;
+using System.Security.Cryptography;
 
 namespace Airport.UI.Controllers
 {
@@ -17,7 +19,8 @@ namespace Airport.UI.Controllers
         IGetCarDetail _getCarDetail;
         IReservationsDAL _reservations;
         ILoginAuthDAL _loginAuth;
-        public PanelController(IMyCarsDAL myCars, IUserDatasDAL userDatas, ILocationsDAL locations, IGetCarDetail getCarDetail,IReservationsDAL reservations, ILoginAuthDAL loginAuth)
+        IUserDatasDAL _user;
+        public PanelController(IMyCarsDAL myCars, IUserDatasDAL userDatas, ILocationsDAL locations, IGetCarDetail getCarDetail,IReservationsDAL reservations, ILoginAuthDAL loginAuth,IUserDatasDAL user)
         {
             _myCars = myCars;
             _userDatas = userDatas;
@@ -25,6 +28,7 @@ namespace Airport.UI.Controllers
             _getCarDetail = getCarDetail;
             _reservations = reservations;   
             _loginAuth = loginAuth;
+            _user = user;
         }
 
         public IActionResult Index()
@@ -60,6 +64,161 @@ namespace Airport.UI.Controllers
                 return BadRequest(ex.ToString());
             }
             
+        }
+
+        [HttpGet("panel/profile")]
+        public IActionResult Profile()
+        {
+            var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+
+            var loginAuth = _loginAuth.SelectByID(userId);
+
+            var user = _user.SelectByID(loginAuth.UserId);
+
+            if (user == null) { return NotFound(); }
+            user.LoginAuth = loginAuth;
+
+            return View(user);
+        }
+
+        [HttpPost("panel/profile")]
+        public IActionResult Profile(UserDatas updateUser)
+        {
+            try
+            {
+                var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+
+                var loginAuth = _loginAuth.SelectByID(userId);
+                var user = _user.SelectByID(loginAuth.UserId);
+
+                if (user == null) { return NotFound(); }
+
+                user.Name = updateUser.Name;
+                user.PhoneNumber = updateUser.PhoneNumber;
+                user.Profession = updateUser.Profession;
+
+                _user.Update(user);
+
+
+                return Json(new { result = 1 });
+            }
+            catch (Exception)
+            {
+                return Json(new { });
+            }
+
+        }
+
+        [HttpGet("panel/settings")]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        [HttpGet("panel/change-password")]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost("panel/change-password", Name = "panelChangePassword")]
+        public IActionResult ChangePassword(string oldPassword, string newPassword)
+        {
+            try
+            {
+                var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+
+                var user = _loginAuth.SelectByID(userId);
+                if (user == null) { return NotFound(); }
+
+                if (GetMD5(oldPassword) == user.Password)
+                {
+                    user.Password = GetMD5(newPassword);
+                    _loginAuth.Update(user);
+                    ViewBag.Message = "success";
+
+                    return View();
+                }
+
+                ViewBag.Message = "wrongPass";
+                return View();
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Error";
+                return View();
+            }
+
+        }
+
+        [HttpGet("panel/bank-account")]
+        public IActionResult BankAccount()
+        {
+            return View();
+        }
+
+        [HttpGet("panel/agreements")]
+        public IActionResult Agreement()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "2")]
+        [HttpGet("panel/my-company")]
+        public IActionResult Company()
+        {
+            var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+            var loginAuth = _loginAuth.SelectByID(userId);
+            var user = _user.SelectByID(loginAuth.UserId);
+            if (user == null) { return NotFound(); }
+            user.LoginAuth = loginAuth;
+            return View(user);
+        }
+
+        [Authorize(Roles = "2")]
+        [HttpPost("panel/my-company")]
+        public IActionResult Company(UserDatas updateUser)
+        {
+            try
+            {
+                var userId = Convert.ToInt32(Request.HttpContext.User.Claims.Where(a => a.Type == ClaimTypes.Sid).Select(a => a.Value).SingleOrDefault());
+
+                var loginAuth = _loginAuth.SelectByID(userId);
+                var user = _user.SelectByID(loginAuth.UserId);
+                if (user == null) { return NotFound(); }
+
+                user.AboutUs = updateUser.AboutUs;
+                user.CompanyPhoneNumber = updateUser.CompanyPhoneNumber;
+                user.CompanyEmail = updateUser.CompanyEmail;
+                user.CompanyWebsite = updateUser.CompanyWebsite;
+                user.Address = updateUser.Address;
+                user.CompanyName = updateUser.CompanyName;
+                user.Country = updateUser.Country;
+                user.Facebook = updateUser.Facebook;
+                user.Linkedin = updateUser.Linkedin;
+
+                _user.Update(user);
+
+                return Json(new { result = 1 });
+            }
+            catch (Exception)
+            {
+                return Json(new { });
+            }
+
+        }
+
+        public static string GetMD5(string value)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] md5Bytes = System.Text.Encoding.Default.GetBytes(value);
+            byte[] cryString = md5.ComputeHash(md5Bytes);
+            string md5Str = string.Empty;
+            for (int i = 0; i < cryString.Length; i++)
+            {
+                md5Str += cryString[i].ToString("X");
+            }
+            return md5Str;
         }
     }
 }
